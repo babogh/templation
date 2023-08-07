99611c482e9786bfa5f2f9cbe81d6e398f88ec98
Amogh <amoghh@uw.edu>
2023-08-07 15:09:09 -0700

    Create homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import bhudda from './bhudda.svg'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Yo</h1>
+      <h1>Welcome to Templation</h1>
+      <img src={bhudda} alt="Bhudda" />
+      <div className="button-container">
+        <button className="create" onClick={() => alert('Feature in progress')}>
+          Create Animation
+        </button>
+        <button className="view" onClick={() => alert('Feature in progress')}>
+          View Animations
+        </button>
+        <button className="monastery" onClick={() => alert('Feature in progress')}>
+          Monastery
+        </button>
+      </div>
     </div>
   );
 }
