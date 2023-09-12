1ce17ca28a625037426eb49e9920d59c7ccfefc6
Amogh <amoghh@uw.edu>
2023-09-12 14:28:31 -0700

    First changes for more aesthetic homepage, animated bhudda, font change

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import Button from './components/Button';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Templation</h1>
-      <img src="/images/bhudda.svg" alt="Bhudda" />
+      <div className="bhudda-container">
+        <img src="/images/bhudda.svg" alt="Bhudda" />
+        <h1>Welcome to Templation</h1>
+      </div>
       <div className="button-container">
       <Button
-          text=""
+          text="Create Animation"
           onClick={() => alert('Feature in progress')}
           position={{ top: '100px', right: '400px' }}
-          svgIcon="./images/createbutton.svg"
         />
         <Button
           text="View Animations"
           onClick={() => alert('Feature in progress')}
-          position={{ top: '200px', left: '400px' }} // Adjust position as needed
+          position={{ top: '100px', left: '400px' }} // Adjust position as needed
         />
         <Button
           text="Monastery"
           onClick={() => alert('Feature in progress')}
-          position={{ top: '700px', right: '930px' }} // Adjust position as needed
+          position={{ top: '100px', right: '930px' }} // Adjust position as needed
         />
       </div>
     </div>
