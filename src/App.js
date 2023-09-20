afbf840dacc9e656dda68169bbb0215d72cc36e8
Amogh <amoghh@uw.edu>
2023-09-19 17:12:10 -0700

    Give HomePage its own files

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import './App.css';
-import Button from './components/Button';
+import React from 'react';
+import './App.css'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <div className="App">
-      <div className="bhudda-container">
-        <img src="/images/bhudda.svg" alt="Bhudda" />
-        <h1>Welcome to Templation</h1>
-      </div>
-      <div className="button-container">
-      <Button
-          text="Create Animation"
-          onClick={() => alert('Feature in progress')}
-          position={{ top: '100px', right: '400px' }}
-        />
-        <Button
-          text="View Animations"
-          onClick={() => alert('Feature in progress')}
-          position={{ top: '100px', left: '400px' }} // Adjust position as needed
-        />
-        <Button
-          text="Monastery"
-          onClick={() => alert('Feature in progress')}
-          position={{ top: '100px', right: '930px' }} // Adjust position as needed
-        />
-      </div>
+      <HomePage/>
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,32 @@
+import '../css/HomePage.css';
+import Button from '../components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bhudda-container">
+        <img src="/images/bhudda.svg" alt="Bhudda" />
+        <h1>Welcome to Templation</h1>
+      </div>
+      <div className="button-container">
+      <Button
+          text="Create Animation"
+          onClick={() => alert('Feature in progress')}
+          position={{ top: '100px', right: '400px' }}
+        />
+        <Button
+          text="View Animations"
+          onClick={() => alert('Feature in progress')}
+          position={{ top: '100px', left: '400px' }} // Adjust position as needed
+        />
+        <Button
+          text="Monastery"
+          onClick={() => alert('Feature in progress')}
+          position={{ top: '100px', right: '930px' }} // Adjust position as needed
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
